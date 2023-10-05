0ecf07cd44514603ef0ba8c659c3307700f7a80d
Vlad <smvk.fox@gmail.com>
2023-10-05 21:02:27 +0300

    logo slider, advantages sections

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,9 @@
 global.$ = global.jQuery = require('jquery');
+import 'slick-carousel';
+
+import logoSlider from "./components/logo-slider";
+
 
 require('./components/menu');
+
+logoSlider();
---
+++ src/js/components/logo-slider.js
@@ -0,0 +1,10 @@
+const logoSlider = () => {
+    // $(document).ready(function() {
+        $('.js--logo-slider').slick({
+            infinite: true,
+            slidesToShow: 7,
+            slidesToScroll: 1,
+        // });
+    });
+}
+export default logoSlider;
