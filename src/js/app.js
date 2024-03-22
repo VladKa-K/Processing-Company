926675d4cdc6ff3d04252993075893cc6959bd8b
Vlad <smvk.fox@gmail.com>
2024-03-22 10:32:59 +0100

    logo slider behavior, styles were added

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,5 @@
 global.$ = global.jQuery = require('jquery');
-import 'slick-carousel';
-
 import logoSlider from "./components/logo-slider";
-
 
 require('./components/menu');
 
--- src/js/components/logo-slider.js
+++ src/js/components/logo-slider.js
@@ -1,10 +1,24 @@
+import Swiper from 'swiper/bundle';
+
 const logoSlider = () => {
-    // $(document).ready(function() {
-        $('.js--logo-slider').slick({
-            infinite: true,
-            slidesToShow: 7,
-            slidesToScroll: 1,
-        // });
+    $(document).ready(function() {
+        const swiper = new Swiper('.js--logo-slider', {
+            slidesPerView: 4,
+            spaceBetween: 15,
+            loop: true,
+            autoplay: {
+                delay: 1,
+            },
+            speed: 7000,
+            breakpoints: {
+                768: {
+                  slidesPerView: 5,
+                },
+                1024: {
+                  slidesPerView: 6,
+                },
+            }
+        });
     });
 }
 export default logoSlider;
